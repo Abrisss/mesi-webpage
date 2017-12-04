2169fa99a18820208b6aaeec32f12e53ee36fc74
Abraham <menyhartabraham@gmail.com>
2017-12-04 22:13:53 +0100

    routing fixed + google verified

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,11 +4,11 @@ import {AboutMeComponent} from './about-me/about-me.component';
 
 
 const routes: Routes = [
-  {path: '', component: AboutMeComponent},
-  {path: 'about-me', component: AboutMeComponent},
-  // {path: 'blog', component: BlogComponent},
-  // {path: 'send-message', component: SendMessageComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: '', component: AboutMeComponent, pathMatch: 'full'},
+  {path: 'about-me', component: AboutMeComponent, pathMatch: 'full'},
+  // {path: 'blog', component: BlogComponent, pathMatch: 'full'},
+  // {path: 'send-message', component: SendMessageComponent, pathMatch: 'full'},
+  {path: 'contact', component: ContactComponent, pathMatch: 'full'}
 ];
 
 export const routing = RouterModule.forRoot(routes);
