c2ac0ef25244b189ae15b17dc942f1abf4d3745a
Abraham <menyhartabraham@gmail.com>
2017-12-08 09:08:03 +0100

    map es meta

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,16 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.css']
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  constructor(private metaService: Meta) {
   }
 
   ngOnInit() {
+    this.metaService.addTag({property: 'description', content: 'Bemutatkozás'});
+  }
+
+  ngOnDestroy() {
+    this.metaService.removeTag('property=\'description\'');
   }
 
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,21 +1,27 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements OnInit, OnDestroy {
 
   lat = 47.505983;
   lng = 19.070909;
   zoom = 15;
   title = 'Rózsa pszichológiai központ';
 
-  constructor() {
+  constructor(private metaService: Meta) {
   }
 
   ngOnInit() {
+    this.metaService.addTag({property: 'description', content: 'Elérhetőség'});
+  }
+
+  ngOnDestroy() {
+    this.metaService.removeTag('property=\'description\'');
   }
 
 }
